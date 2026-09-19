ea39e76dd7801d75d2a5da9fc5686066f2e3f067
Vansh Kamboj <vanshkamboj2303@gmail.com>
2026-09-19 16:00:46 +0530

    Add Render health check endpoint

--- portfolio_backend/routes/web.php
+++ portfolio_backend/routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
+
+Route::get('/health', function () {
+    return response()->json([
+        'status' => 'ok'
+    ]);
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
